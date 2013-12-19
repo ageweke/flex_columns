018e0f9b32bea1b3cbca75595535a3aedab729d7
Andrew Geweke <andrew@geweke.org>
2013-12-19 11:49:39 -0800

    Spec and documentation for IncludeFlexColumns.

--- lib/flex_columns/including/include_flex_columns.rb
+++ lib/flex_columns/including/include_flex_columns.rb
@@ -2,10 +2,71 @@ require 'active_support/concern'
 
 module FlexColumns
   module Including
+    # IncludeFlexColumns defines the methods on ActiveRecord::Base that get triggered when you say
+    # 'include_flex_columns_from' on an ActiveRecord model. Note, however, that it is not simply directly included
+    # into ActiveRecord::Base; rather, it's included only when you actually make that declaration, and included
+    # into the specific model class itself. (This helps avoid pollution or conflict on any ActiveRecord models that
+    # have not used this functionality.)
+    #
+    # This module works in a pretty different way from FlexColumns::HasFlexColumns, which is the corresponding module
+    # that gets included when you declare a flex column with <tt>flex_column :foo do ... end</tt>. That module builds
+    # up an object representation of the flex column itself and of all its fields, and then holds onto these objects
+    # and uses them to do its work. This module, on the other hand, actively and aggressively defines the appropriate
+    # methods when you call #include_flex_columns_from, but does not create or hold onto any object representation of
+    # the included columns. This is for two reasons: first off, there's a lot more complexity in defining a flex
+    # column itself than in simply including one. Secondly, and more subtly, defining a flex column is a process with
+    # a decided start and end -- the contents of the block passed to +flex_column+. Including fields, however, is a
+    # component part of a class that's defined using the Ruby +class+ keyword, and which can get reopened and redefined
+    # at any given time. Thus, we really have no choice but to aggressively define methods when
+    # +include_flex_columns_from+ is called; holding onto an object representation would largely just ensure that that
+    # object representation grew incorrect over time in development mode, as columns get defined and redefined over
+    # time.
+    #
+    # (A corollary of this is that, in Rails development mode, depending on how classes get reloaded, it's possible that
+    # if you remove an +include_flex_columns_from+ declaration from a model, the defined methods won't actually
+    # disappear until you restart your server. There's really not much we can do about this, since there's no Ruby hook
+    # that says "someone is defining methods on class X" -- nor would one make any sense, since you can re-open classes
+    # at any time and as many times as you want in Ruby.)
+    #
+    # In comments below, we're working with the following example:
+    #
+    #    class UserDetail < ActiveRecord::Base
+    #      self.primary_key = :user_id
+    #      belongs_to :user
+    #
+    #      flex_column :details do
+    #        field :background_color
+    #        field :likes_peaches
+    #      end
+    #    end
+    #
+    #    class User < ActiveRecord::Base
+    #      has_one :detail
+    #
+    #      include_flex_columns_from :detail
+    #    end
     module IncludeFlexColumns
+      # Make sure our ClassMethods module gets +extend+ed into any class that +include+s us.
       extend ActiveSupport::Concern
 
+      # This is the method that gets called by generated delegated methods, and called in order to retrieve the
+      # correct flex-column object for a column. In other words, the generated method User#background_color looks
+      # something like:
+      #
+      #     def background_color
+      #       flex_column_object = _flex_column_object_for(:details)
+      #       flex_column_object.background_color
+      #     end
+      #
+      # (We do this partially so that the exact same method definition works for UserDetail and for User; _i.e._,
+      # whether you're running on a class that itself has a flex column, or on a class that simply is including another
+      # class's flex columns, #_flex_column_object_for will get you the right object.)
+      #
+      # There's only one nasty case to deal with here: what if User has its own flex column +detail+? In such a case, we
+      # want to return the flex-column object that's defined for the column the class has itself, not for the one it's
+      # including.
       def _flex_column_object_for(column_name)
+        # This is the "nasty case", above.
         begin
           return super(column_name)
         rescue NoMethodError
@@ -14,31 +75,47 @@ module FlexColumns
           # ok
         end
 
+        # Fetch the association that this column is included from.
         association = self.class._flex_column_is_included_from(column_name)
 
         if association
+          # Get the associated object. We automatically will build the associated object, if necessary; this is so that
+          # you don't have to either actively create associated objects ahead of time, just in case you need them later,
+          # or litter your code with checks to see if those objects exist already or not.
           associated_object = send(association) || send("build_#{association}")
           return associated_object.send(column_name)
         else
-          raise FlexColumns::Errors::NoSuchColumnError.new(%{Class #{name} knows nothing of a flex column named #{column_name.inspect}.})
+          raise FlexColumns::Errors::NoSuchColumnError.new(%{Class #{self.class.name} knows nothing of a flex column named #{column_name.inspect}.})
         end
       end
 
       module ClassMethods
+        # The DynamicMethodsModule on which we define all methods generated by included flex columns.
         def _flex_columns_include_flex_columns_dynamic_methods_module
           @_flex_columns_include_flex_columns_dynamic_methods_module ||= FlexColumns::Util::DynamicMethodsModule.new(self, :FlexColumnsIncludedColumnsMethods)
         end
 
-        def _flex_column_included_from(flex_column_name, association_name)
-          @_included_flex_columns_map ||= { }
-          @_included_flex_columns_map[flex_column_name] = association_name
-        end
-
+        # Returns the name of the association from which a flex column of the given name was included.
         def _flex_column_is_included_from(flex_column_name)
           @_included_flex_columns_map[flex_column_name]
         end
 
+        # Includes methods from the given flex column or flex columns into this class.
+        #
+        # +args+ should be a list of association names from which you want to include columns. It can also end in an
+        # options Hash, which can contain:
+        #
+        # [:prefix] If set, included method names will be prefixed with the given string (followed by an underscore).
+        #           If not set, the prefix defined on each flex column, if any, will be used; you can override this by
+        #           explicitly passing +nil+ here.
+        # [:visibility] If set to +:private+, included methods will be marked +private+, meaning they can only be
+        #               accessed from inside this model. This can be used to ensure random code across your system
+        #               can't directly manipulate flex-column fields.
+        # [:delegate] If set to +false+ or +nil+, then only the method that accesses the flex column itself (above,
+        #             User#details) will be created; other methods (User#background_color, User#likes_peaches) will
+        #             not be automatically delegated.
         def include_flex_columns_from(*args, &block)
+          # Grab our options, and validate them as necessary...
           options = args.pop if args[-1] && args[-1].kind_of?(Hash)
           options ||= { }
 
@@ -61,7 +138,9 @@ module FlexColumns
 
           @_included_flex_columns_map ||= { }
 
+          # Iterate through each association...
           association_names.each do |association_name|
+            # Get the association and make sure it's of the right type...
             association = reflect_on_association(association_name.to_sym)
             unless association
               raise ArgumentError, %{You asked #{self.name} to include flex columns from association #{association_name.inspect},
@@ -78,12 +157,14 @@ module FlexColumns
   there is no way to know which target object to include the data from.}
             end
 
+            # Grab the target model class, and make sure it has one or more flex columns...
             target_class = association.klass
             if (! target_class.respond_to?(:has_any_flex_columns?)) || (! target_class.has_any_flex_columns?)
               raise ArgumentError, %{You asked #{self.name} to include flex columns from association #{association_name.inspect},
   but the target class of that association, #{association.klass.name}, has no flex columns defined.}
             end
 
+            # Call through and tell those flex columns to create the appropriate methods.
             target_class._all_flex_column_names.each do |flex_column_name|
               @_included_flex_columns_map[flex_column_name] = association_name
 
---
+++ spec/flex_columns/unit/including/include_flex_columns_spec.rb
@@ -0,0 +1,144 @@
+require 'flex_columns'
+
+describe FlexColumns::Including::IncludeFlexColumns do
+  before :each do
+    @klass = Class.new { include FlexColumns::Including::IncludeFlexColumns }
+  end
+
+  describe "#_flex_column_object_for" do
+    it "should return the superclass value if it has one" do
+      superclass = Class.new do
+        def _flex_column_object_for(column_name)
+          "XX#{column_name}YY"
+        end
+      end
+
+      klass = Class.new(superclass) do
+        include FlexColumns::Including::IncludeFlexColumns
+      end
+
+      instance = klass.new
+      instance._flex_column_object_for("foobar").should == "XXfoobarYY"
+    end
+
+    it "should raise an error if there is no appropriate association" do
+      allow(@klass).to receive(:_flex_column_is_included_from).with(:foo).and_return(nil)
+      allow(@klass).to receive(:name).with().and_return("klassname")
+      instance = @klass.new
+
+      lambda { instance._flex_column_object_for(:foo) }.should raise_error(FlexColumns::Errors::NoSuchColumnError, /klassname.*foo/i)
+    end
+
+    it "should invoke the right method on the right object if the association is already there" do
+      allow(@klass).to receive(:_flex_column_is_included_from).with(:colname).and_return(:assocname)
+      instance = @klass.new
+
+      association_object = double("association_object")
+      allow(instance).to receive(:assocname).with().and_return(association_object)
+      allow(association_object).to receive(:colname).and_return(:baz)
+
+      instance._flex_column_object_for(:colname).should == :baz
+    end
+
+    it "should create the associated object if necessary" do
+      allow(@klass).to receive(:_flex_column_is_included_from).with(:colname).and_return(:assocname)
+      instance = @klass.new
+
+      association_object = double("association_object")
+      allow(instance).to receive(:assocname).with().and_return(nil)
+      allow(instance).to receive(:build_assocname).with().and_return(association_object)
+      allow(association_object).to receive(:colname).and_return(:baz)
+
+      instance._flex_column_object_for(:colname).should == :baz
+    end
+  end
+
+  it "should return a singular DynamicMethodsModule on _flex_columns_include_flex_columns_dynamic_methods_module" do
+    dmm = double("dmm")
+    expect(FlexColumns::Util::DynamicMethodsModule).to receive(:new).once.with(@klass, :FlexColumnsIncludedColumnsMethods).and_return(dmm)
+    @klass._flex_columns_include_flex_columns_dynamic_methods_module.should be(dmm)
+    @klass._flex_columns_include_flex_columns_dynamic_methods_module.should be(dmm)
+  end
+
+  describe "#include_flex_columns_from" do
+    before :each do
+      @assoc1 = double("assoc1")
+      allow(@assoc1).to receive(:name).with().and_return(:assoc1)
+      allow(@assoc1).to receive(:macro).with().and_return(:has_one)
+      @assoc2 = double("assoc2")
+      allow(@assoc2).to receive(:name).with().and_return(:assoc2)
+      allow(@assoc2).to receive(:macro).with().and_return(:belongs_to)
+
+      allow(@klass).to receive(:reflect_on_association).with(:assoc1).and_return(@assoc1)
+      allow(@klass).to receive(:reflect_on_association).with(:assoc2).and_return(@assoc2)
+
+      allow(@klass).to receive(:reflect_on_all_associations).with().and_return([ @assoc1, @assoc2 ])
+    end
+
+    it "should validate its options properly" do
+      lambda { @klass.include_flex_columns_from(:foo, :bar => :baz) }.should raise_error(ArgumentError)
+      lambda { @klass.include_flex_columns_from(:foo, :prefix => 123) }.should raise_error(ArgumentError)
+      lambda { @klass.include_flex_columns_from(:foo, :visibility => :bonk) }.should raise_error(ArgumentError)
+      lambda { @klass.include_flex_columns_from(:foo, :delegate => :yes) }.should raise_error(ArgumentError)
+    end
+
+    it "should raise if there is no such association" do
+      allow(@klass).to receive(:reflect_on_association).with(:foo).and_return(nil)
+      lambda { @klass.include_flex_columns_from(:foo) }.should raise_error(ArgumentError, /foo.*assoc1.*assoc2/mi)
+    end
+
+    it "should raise if the association is of the wrong type" do
+      allow(@assoc1).to receive(:macro).with().and_return(:has_many)
+      lambda { @klass.include_flex_columns_from(:assoc1) }.should raise_error(ArgumentError, /assoc1.*has_many/mi)
+    end
+
+    it "should raise if the target class does not respond to #has_any_flex_columns?" do
+      association_class = double("association_class")
+      allow(association_class).to receive(:name).with().and_return("acname")
+      allow(@assoc2).to receive(:klass).with().and_return(association_class)
+      allow(association_class).to receive(:respond_to?).with(:has_any_flex_columns?).and_return(false)
+
+      lambda { @klass.include_flex_columns_from(:assoc2) }.should raise_error(ArgumentError, /assoc2.*acname/mi)
+    end
+
+    it "should raise if the target class returns false from #has_any_flex_columns?" do
+      association_class = double("association_class")
+      allow(association_class).to receive(:name).with().and_return("acname")
+      allow(@assoc2).to receive(:klass).with().and_return(association_class)
+      allow(association_class).to receive(:has_any_flex_columns?).with().and_return(false)
+
+      lambda { @klass.include_flex_columns_from(:assoc2) }.should raise_error(ArgumentError, /assoc2.*acname/mi)
+    end
+
+    it "should call through to the target flex-column class with provided options" do
+      dmm = double("dmm")
+      expect(FlexColumns::Util::DynamicMethodsModule).to receive(:new).once.with(@klass, :FlexColumnsIncludedColumnsMethods).and_return(dmm)
+
+      ac1 = double("ac1")
+      allow(@assoc1).to receive(:klass).with().and_return(ac1)
+      allow(ac1).to receive(:has_any_flex_columns?).with().and_return(true)
+      allow(ac1).to receive(:_all_flex_column_names).with().and_return([ :ac1fc1, :ac1fc2 ])
+
+      ac1fc1_fcc = double("ac1fc1_fcc")
+      expect(ac1fc1_fcc).to receive(:include_fields_into).once.ordered.with(dmm, :assoc1, @klass, :prefix => 'abz', :delegate => false, :visibility => :private)
+      allow(ac1).to receive(:_flex_column_class_for).with(:ac1fc1).and_return(ac1fc1_fcc)
+      ac1fc2_fcc = double("ac1fc2_fcc")
+      expect(ac1fc2_fcc).to receive(:include_fields_into).once.ordered.with(dmm, :assoc1, @klass, :prefix => 'abz', :delegate => false, :visibility => :private)
+      allow(ac1).to receive(:_flex_column_class_for).with(:ac1fc2).and_return(ac1fc2_fcc)
+
+      ac2 = double("ac2")
+      allow(@assoc2).to receive(:klass).with().and_return(ac2)
+      allow(ac2).to receive(:has_any_flex_columns?).with().and_return(true)
+      allow(ac2).to receive(:_all_flex_column_names).with().and_return([ :ac2fc1, :ac2fc2 ])
+
+      ac2fc1_fcc = double("ac2fc1_fcc")
+      expect(ac2fc1_fcc).to receive(:include_fields_into).once.ordered.with(dmm, :assoc2, @klass, :prefix => 'abz', :delegate => false, :visibility => :private)
+      allow(ac2).to receive(:_flex_column_class_for).with(:ac2fc1).and_return(ac2fc1_fcc)
+      ac2fc2_fcc = double("ac2fc2_fcc")
+      expect(ac2fc2_fcc).to receive(:include_fields_into).once.ordered.with(dmm, :assoc2, @klass, :prefix => 'abz', :delegate => false, :visibility => :private)
+      allow(ac2).to receive(:_flex_column_class_for).with(:ac2fc2).and_return(ac2fc2_fcc)
+
+      @klass.include_flex_columns_from(:assoc1, :assoc2, :prefix => 'abz', :delegate => false, :visibility => :private)
+    end
+  end
+end
